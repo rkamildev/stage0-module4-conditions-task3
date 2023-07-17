de4d4c8b26277497c1309f4af29a7d684bbd7e53
rkamildev <rkamil.int@gmail.com>
2023-07-18 01:08:23 +0500

    Module4 task 3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ( !(character >= 65 && character <= 90) && !(character >= 97 && character <= 122) )
+            System.out.println("wrong alphabet!");
+        else if (character == 'A' || character == 'a' ||
+                 character == 'E' || character == 'e' ||
+                 character == 'I' || character == 'i' ||
+                 character == 'O' || character == 'o' ||
+                 character == 'U' || character == 'u' ||
+                 character == 'Y' || character == 'y')
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
     }
 }
